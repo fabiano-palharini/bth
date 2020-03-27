f861d3f7d16d868e21e14ca0813f130c00005607
Palharini <fabianofalco@gmail.com>
2020-03-27 19:50:13 -0300

    adding validations with celebrate and joi on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const {errors} = require('celebrate');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors()); //non-prod
 // app.use(cors({origin: 'http://myapp.com})); //prod
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 
 
